ef6f846558f79805c07fa0e03f162c5fceee8b08
Arghadeep Chakraborty <argho.chakr@gmail.com>
2024-04-05 04:43:12 -0500

    Initial Commit

---
+++ TokenTrend/TokenTrendApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenTrendApp.swift
+//  TokenTrend
+//
+//  Created by Arghadeep Chakraborty on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TokenTrendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
